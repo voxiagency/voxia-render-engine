63c4b618b53d0bb14fc2ae1397ac6a20369ef11a
voxiagency <voxiagency@gmail.com>
2025-11-01 14:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,4 +41,6 @@ app.post('/render', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('🟢 VoxIA Render Engine activo en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🟢 VoxIA Render Engine activo en puerto ${PORT}`));
+
